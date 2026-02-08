9783d7c36ea09f2a42c8938d921f8c9f177789cc
Sandesh Shinde <shindesandesh2398@gmail.com>
2026-02-09 01:31:15 +0530

    first commit

---
+++ src/main/java/com/example/linkscape/LinkscapeBlipsApplication.java
@@ -0,0 +1,15 @@
+package com.example.linkscape;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkscapeBlipsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkscapeBlipsApplication.class, args);
+		System.out.println("FIRST COMMIT");
+
+	}
+
+}
---
+++ src/test/java/com/example/linkscape/LinkscapeBlipsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.linkscape;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkscapeBlipsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
